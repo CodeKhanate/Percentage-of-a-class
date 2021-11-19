d727cc1513d0febad119bb5fcfc8c8b4e57b6280
khorezmi <dlaregareleg2001@gmail.com>
2021-11-19 23:45:12 +0800

    first commit

---
+++ MidtermExam.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package midtermexam;
+
+import java.util.Scanner;
+
+public class MidtermExam {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+      float males;
+      float females;
+      int NumOfStudent = 53;
+      float formulaMales, formulaFemales;
+
+      System.out.print("Enter the number of males: ");
+      males = input.nextFloat();
+      
+      System.out.print("Enter the number of Females: ");
+      females = input.nextFloat();
+      
+      formulaMales = males / NumOfStudent * 100;
+      formulaFemales = females / NumOfStudent * 100;
+      
+      System.out.println("Number of Students = " + NumOfStudent);
+      System.out.println("males = " + Math.round(formulaMales * 100.0) / 100.0 + "%");
+      System.out.println("females = " + Math.round(formulaFemales * 100.0) / 100.0 + "%");
+    }
+    
+}
